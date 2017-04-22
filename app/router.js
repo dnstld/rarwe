b0412f3587d26baa0394cb0540a281fc1d9ccced
Denis Toledo <contato@denistoledo.com.br>
2017-04-22 20:42:47 -0300

    Chapter 4: Routing | 40

--- app/router.js
+++ app/router.js
@@ -2,10 +2,13 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  baseURL: config.baseURL
 });
 
 Router.map(function() {
+  this.route('bands');
+  this.route('songs');
 });
 
 export default Router;
